abb160f6769a8d9acb8b9468be1dab344fdafc6b
vignan.kandela <vignan.kandela@gmail.com>
2025-05-01 19:34:34 +0000

    test index

--- src/test/java/hello/HelloControllerTest.java
+++ src/test/java/hello/HelloControllerTest.java
@@ -2,13 +2,18 @@ package hello;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest
 public class HelloControllerTest {
+
+    @Autowired
+    private HelloController controller;
 
     @Test
     public void testIndex() {
-        HelloController controller = new HelloController();
         String result = controller.index();
-        assertTrue(result.contains("JAVA application deployed on EC2 with the latest code version using Jenkins pipeline – from Vignan."));
+        assertTrue(result.contains("JAVA application deployed on EC2 with the latest code version using Jenkins pipeline – from Vignan.")); // Assuming the static part of the message remains constant
     }
 }
