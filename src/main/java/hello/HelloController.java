dbb61324510871640b0a09542b255150df5cff02
vignan.kandela <vignan.kandela@gmail.com>
2025-05-01 19:24:52 +0000

    test

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,13 +1,39 @@
 package hello;
 
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    
+    @RequestMapping("/")
+    public String index() {
+        // Get the host machine's IPv4 address
+        String ipAddress = "";
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            ipAddress = inetAddress.getHostAddress();
+        } catch (java.net.UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        // Return the greeting message with the IPv4 address
+        return "JAVA application deployed on EC2 with the latest code version using Jenkins pipeline – from Vignan."   + ipAddress;
+        
+
+    }
+    
+    @Test
+    public void testIndex() {
+        HelloController controller = new HelloController();
+        String result = controller.index();
+        assertTrue(result.contains("JAVA application deployed on EC2 with the latest code version using Jenkins pipeline – from Vignan."));
+    }
+    
+}
     
     @RequestMapping("/")
     public String index() {
