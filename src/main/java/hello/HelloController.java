12bcca11c61a1db25bd605420c80f827c6d25ce6
vignan2020 <88151516+vickydevo@users.noreply.github.com>
2025-09-18 16:42:31 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -18,6 +18,6 @@ public class HelloController {
         }
 
         // Return the greeting message with the IPv4 address
- return "JAVA application deployed on EC2 with the latest code version using Jenkins pipeline... from Vignan. IP Address: " + ipAddress;
+ return "JAVA application deployed on Docker container 18-aug 2025... By Vignan. IP Address: " + ipAddress;
     }
 }
