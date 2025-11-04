443fb3a0b4aacf65484b1d90d2a9ae4bc34cb975
vignan2020 <88151516+vickydevo@users.noreply.github.com>
2025-11-04 12:01:34 +0530

    Update HelloController.java

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -18,6 +18,6 @@ public class HelloController {
         }
 
         // Return the greeting message with the IPv4 address
- return "JAVA application deployed on docker 04-Nov... By Vignan. IP Address: " + ipAddress;
+ return "JAVA application deployed on DOCKER CONTAINER 04-Nov... By Vignan. IP Address: " + ipAddress;
     }
 }
